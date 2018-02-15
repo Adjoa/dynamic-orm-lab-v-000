a93cbf9d0c859933cb5a1ab30edf190e8b6be9f4
Adjoa Darien <adjoa.darien@gmail.com>
2018-02-15 17:27:45 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,9 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
-  # def attr_accessor
-    self.column_names.each do |col_name|
-      attr_accessor col_name.to_sym
-    end
-  # end
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
 
 end
